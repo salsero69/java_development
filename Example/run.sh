9661676dacf60ec426b169d8d512a8fda532ec0a
Don Vaillancourt <vaillancourt.don@gmail.com>
2014-11-26 04:02:00 +0000

    Execute bash file

---
+++ Example/run.sh
@@ -0,0 +1,2 @@
+mvn install
+java -jar /home/ubuntu/workspace/Example/target/Example-1.0-SNAPSHOT.jar
--- Example/src/main/java/com/dmv/example/App.java
+++ Example/src/main/java/com/dmv/example/App.java
@@ -4,10 +4,12 @@ package com.dmv.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        System.out.println( "How are you?" );
     }
 }
