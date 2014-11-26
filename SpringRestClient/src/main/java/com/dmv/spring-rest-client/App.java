01bd46134e5e0d1927cc481c57c8c37a7a52cf32
Don Vaillancourt <vaillancourt.don@gmail.com>
2014-11-26 04:12:08 +0000

    Spring Framework rest client

---
+++ SpringRestClient/src/main/java/com/dmv/spring-rest-client/App.java
@@ -0,0 +1,13 @@
+package com.dmv.spring-rest-client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
